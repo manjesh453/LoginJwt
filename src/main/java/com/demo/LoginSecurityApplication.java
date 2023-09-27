a43e510f8b978321ebff6342d636b819c589ae80
manjesh453 <rayamajhimanjesh41@gmail.com>
2023-09-27 08:51:34 +0545

    Initializing project

---
+++ src/main/java/com/demo/LoginSecurityApplication.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LoginSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSecurityApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
